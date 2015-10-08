45984985748f0b68b0794913c69d0275bf421e94
Alexander Kerner <ak@silico-sciences.com>
2015-10-08 22:52:59 +0200

    JavaDoc.

--- src/main/java/org/jcamp/spectrum/IRSpectrum.java
+++ src/main/java/org/jcamp/spectrum/IRSpectrum.java
@@ -7,8 +7,6 @@
  ******************************************************************************/
 package org.jcamp.spectrum;
 
-import org.jcamp.math.IInterval1D;
-import org.jcamp.math.IOrderedArray1D;
 import org.jcamp.math.IrregularAxisMap;
 import org.jcamp.math.IrregularGrid1D;
 import org.jcamp.math.LinearAxisMap;
@@ -21,87 +19,90 @@ import org.jcamp.units.Unit;
  * 
  * @author Thomas Weber
  */
-public class IRSpectrum
-  extends OpticalSpectrum1D {
-  
-  /** for serialization. */
-  private static final long serialVersionUID = 2377464639384336853L;
+public class IRSpectrum extends OpticalSpectrum1D {
 
-  private final static double[] DEFAULT_WAVENUMBERS =
-      new double[] { 4000.0, 3500.0, 3000.0, 2500.0, 2000.0, 1800, 1600, 1400, 1200, 1000, 800, 600, 400 };
-  
-  private final static IrregularGrid1D DEFAULT_XGRID = new IrregularGrid1D(DEFAULT_WAVENUMBERS);
-  
-  public final static Unit DEFAULT_XUNIT = CommonUnit.perCM;
-  
-  public final static Unit DEFAULT_YUNIT = CommonUnit.intensity;
+	/** for serialization. */
+	private static final long serialVersionUID = 2377464639384336853L;
 
-  /**
+	private final static double[] DEFAULT_WAVENUMBERS = new double[] { 4000.0,
+			3500.0, 3000.0, 2500.0, 2000.0, 1800, 1600, 1400, 1200, 1000, 800,
+			600, 400 };
+
+	private final static IrregularGrid1D DEFAULT_XGRID = new IrregularGrid1D(
+			DEFAULT_WAVENUMBERS);
+
+	public final static Unit DEFAULT_XUNIT = CommonUnit.perCM;
+
+	public final static Unit DEFAULT_YUNIT = CommonUnit.intensity;
+
+	/**
    */
-  protected IRSpectrum() {
-    super();
-  }
+	protected IRSpectrum() {
+		super();
+	}
 
-  /**
-   * standard ctor.
-   * 
-   * @param x org.jcamp.spectrum.IOrderedDataArray1D
-   * @param y org.jcamp.spectrum.IDataArray1D
-   */
-  public IRSpectrum(IOrderedDataArray1D x, IDataArray1D y) {
-    super(x, y);
-  }
+	/**
+	 * standard ctor.
+	 * 
+	 * @param x
+	 *            org.jcamp.spectrum.IOrderedDataArray1D
+	 * @param y
+	 *            org.jcamp.spectrum.IDataArray1D
+	 */
+	public IRSpectrum(IOrderedDataArray1D x, IDataArray1D y) {
+		super(x, y);
+	}
 
-  /**
-   * standard ctor.
-   * 
-   * @param x org.jcamp.spectrum.IOrderedDataArray1D
-   * @param y org.jcamp.spectrum.IDataArray1D
-   * @param fullSpectrum boolean
-   */
-  public IRSpectrum(IOrderedDataArray1D x, IDataArray1D y, boolean fullSpectrum) {
-    super(x, y, fullSpectrum);
-    xAxisMap = new IrregularAxisMap(xData, DEFAULT_XGRID);
-    yAxisMap = new LinearAxisMap(yData);
-  }
+	/**
+	 * standard ctor.
+	 * 
+	 * @param x
+	 *            org.jcamp.spectrum.IOrderedDataArray1D
+	 * @param y
+	 *            org.jcamp.spectrum.IDataArray1D
+	 * @param fullSpectrum
+	 *            boolean
+	 */
+	public IRSpectrum(IOrderedDataArray1D x, IDataArray1D y,
+			boolean fullSpectrum) {
+		super(x, y, fullSpectrum);
+		xAxisMap = new IrregularAxisMap(xData, DEFAULT_XGRID);
+		yAxisMap = new LinearAxisMap(yData);
+	}
 
-  /**
-   * cloning.
-   * 
-   * @return java.lang.Object
-   */
-  @Override
-  public Object clone() {
-    IRSpectrum spectrum = (IRSpectrum) super.clone();
-    this.xAxisMap = new IrregularAxisMap((IOrderedArray1D) xData, DEFAULT_XGRID);
-    return spectrum;
-  }
+	@Override
+	public IRSpectrum clone() {
+		IRSpectrum spectrum = (IRSpectrum) super.clone();
+		this.xAxisMap = new IrregularAxisMap(xData, DEFAULT_XGRID);
+		return spectrum;
+	}
 
-  /**
-   * gets spectrum ID
-   * @return int
-   */
-  @Override
-  public int getIdentifier() {
-    return ISpectrumIdentifier.IR;
-  }
+	/**
+	 * gets spectrum ID
+	 * 
+	 * @return int
+	 */
+	@Override
+	public int getIdentifier() {
+		return ISpectrumIdentifier.IR;
+	}
 
-  /**
-   * isSameType method comment.
-   */
-  @Override
-  public boolean isSameType(Spectrum otherSpectrum) {
-    if (otherSpectrum instanceof IRSpectrum)
-      return true;
-    return false;
-  }
+	/**
+	 * isSameType method comment.
+	 */
+	@Override
+	public boolean isSameType(Spectrum otherSpectrum) {
+		if (otherSpectrum instanceof IRSpectrum)
+			return true;
+		return false;
+	}
 
-  /**
-   * sets full view range on x axis.
-   */
-  @Override
-  public void setXFullViewRange(Range1D.Double dataRange) {
-    IrregularGrid1D newGrid = new IrregularGrid1D(DEFAULT_XGRID, dataRange);
-    this.xAxisMap = new IrregularAxisMap((IInterval1D) xData, newGrid);
-  }
+	/**
+	 * sets full view range on x axis.
+	 */
+	@Override
+	public void setXFullViewRange(Range1D.Double dataRange) {
+		IrregularGrid1D newGrid = new IrregularGrid1D(DEFAULT_XGRID, dataRange);
+		this.xAxisMap = new IrregularAxisMap(xData, newGrid);
+	}
 }
--- src/main/java/org/jcamp/spectrum/Spectrum.java
+++ src/main/java/org/jcamp/spectrum/Spectrum.java
@@ -24,6 +24,7 @@ import org.jcamp.spectrum.notes.NoteDescriptor;
  * 
  * @see Spectrum1D
  * @see Spectrum2D
+ * @see IRSpectrum
  * 
  * @author Thomas Weber
  * 

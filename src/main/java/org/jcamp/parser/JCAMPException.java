a18710e65d3524bdf3a9af97b39c0a2365d70e5c
Alexander Kerner <ak@silico-sciences.com>
2015-10-08 16:31:22 +0200

    JCAMPException: Add super type constructors.

--- src/main/java/org/jcamp/parser/JCAMPException.java
+++ src/main/java/org/jcamp/parser/JCAMPException.java
@@ -1,33 +1,40 @@
-/*******************************************************************************
- * Copyright (c) 2015.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
+/**
+ * *****************************************************************************
+ * Copyright (c) 2015. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the GNU Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- ******************************************************************************/
+ * ****************************************************************************
+ */
 package org.jcamp.parser;
 
 /**
- * exceptions thrown at JCAMP-DX parsing.
- * 
+ * Exceptions thrown at JCAMP-DX parsing.
+ *
  * @author Thomas Weber
+ * @author <a href="mailto:alexander.kerner@silico-sciences.com">Alexander
+ *         Kerner</a>
  */
 public class JCAMPException
-  extends Exception {
-  
-  /** for serialization. */
-  private static final long serialVersionUID = -4773819366174671655L;
-  /**
-   * JCAMPException constructor comment.
-   */
-  public JCAMPException() {
-    super();
-  }
-  /**
-   * JCAMPException constructor comment.
-   * @param s java.lang.String
-   */
-  public JCAMPException(String s) {
-    super(s);
-  }
+		extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7234910260603380490L;
+
+	public JCAMPException() {
+	}
+
+	public JCAMPException(String message) {
+		super(message);
+	}
+
+	public JCAMPException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public JCAMPException(Throwable cause) {
+		super(cause);
+	}
 }
